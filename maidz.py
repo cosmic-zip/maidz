71e2327f4e80da71a0e2897c6c2fa6fe803d82f5
prism-zip <36008397+prism-zip@users.noreply.github.com>
2024-06-17 04:28:34 -0300

    black

--- maidz.py
+++ maidz.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 # -----------------------------------------------------------------------------#
-#   MaidZ Cybersecurity companion                                              # 
-#   Copyright (C) 2024  Prism-zip                                              # 
-#                                                                              # 
-#                                                                              # 
-#   This program is free software: you can redistribute it and/or modify       # 
-#   it under the terms of the GNU Affero General Public License as published   # 
-#   by the Free Software Foundation, either version 3 of the License, or       # 
-#   (at your option) any later version.                                        # 
-#                                                                              # 
-#   This program is distributed in the hope that it will be useful,            # 
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of             # 
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              # 
-#   GNU Affero General Public License for more details.                        # 
-#                                                                              # 
-#   You should have received a copy of the GNU Affero General Public License   # 
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.     # 
+#   MaidZ Cybersecurity companion                                              #
+#   Copyright (C) 2024  Prism-zip                                              #
+#                                                                              #
+#                                                                              #
+#   This program is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU Affero General Public License as published   #
+#   by the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU Affero General Public License for more details.                        #
+#                                                                              #
+#   You should have received a copy of the GNU Affero General Public License   #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
 # -----------------------------------------------------------------------------#
 
 
 import json, os, sys, time, re
 
+
 def sakuya():
     os.system("chafa -s 90x90 sakuya_izayoi.png")
-    print("\n\033[1m\t[maidz] Cybersecurity companion\n\tUse maidz help to see all options\033[0m\n")
-    
+    print(
+        "\n\033[1m\t[maidz] Cybersecurity companion\n\tUse maidz help to see all options\033[0m\n"
+    )
+
+
 def import_bank():
     try:
         with open("bank.json", "r") as bank:
@@ -32,16 +36,17 @@ def import_bank():
     except json.JSONDecodeError as e:
         print(f"JSON::ERROR {e}")
 
+
 def puts(string: str, color: str = ""):
     color_emojis = {
-        'purple': '🟣',
-        'red': '🔴',
-        'green': '🟢',
-        'yellow': '🟡',
-        'blue': '🔵',
-        'orange': '🟠',
-        'white': '⚪',
-        'black': '⚫',
+        "purple": "🟣",
+        "red": "🔴",
+        "green": "🟢",
+        "yellow": "🟡",
+        "blue": "🔵",
+        "orange": "🟠",
+        "white": "⚪",
+        "black": "⚫",
     }
     emoji = ""
     if color != "":
@@ -51,28 +56,27 @@ def puts(string: str, color: str = ""):
 
 def key_value(term: str, args: list):
 
-
     if len(args) < 1:
         print("args are to small")
-        
+
     for index, item in enumerate(args):
         if item == term:
             if index + 1 >= len(args):
                 puts("Incomplete key-value constrain :: return empty")
                 return ""
             # print(args[index +1])
-            return args[index +1]
+            return args[index + 1]
 
     puts(f"no value found for key: {term} :: return empty")
     return ""
-    
-    
+
+
 def query(data: dict, args: list):
-    
+
     name = ""
     content = ""
     description = ""
-    pattern = r'\s*@@(\w+)\s*'
+    pattern = r"\s*@@(\w+)\s*"
 
     for ctn in data:
         if ctn["name"] == args[1]:
@@ -83,25 +87,22 @@ def query(data: dict, args: list):
     if not content:
         puts(f"bind not found for {args[1]}")
         return None
-    
-    
+
     matches = re.findall(pattern, content)
     for match in matches:
         key = match.strip()
         value = key_value(f"--{key}", args)
-        content = content.replace(f'@@{key}', value)
-    
+        content = content.replace(f"@@{key}", value)
+
     # print(content)
-    return {
-        "name": name,
-        "command": content,
-        "description": description
-    }
-    
+    return {"name": name, "command": content, "description": description}
+
+
 def exec(command: str) -> int:
     puts(command["name"])
     print(command["command"])
     return 1
+
 
 def exec_batch(chunk: dict, args: list, delay: int = 0):
 
@@ -111,17 +112,19 @@ def exec_batch(chunk: dict, args: list, delay: int = 0):
             time.sleep(delay)
         print(out)
 
+
 def help(verbose: bool = False):
 
     data = import_bank()
     data = data["general"]
 
     for d in data:
-        puts(d['name'], "yellow")
+        puts(d["name"], "yellow")
         puts(f"\n\t{d['description']}")
         if verbose:
             print(f"\n\t{d['command']}\n")
         print("")
+
 
 def install_deps():
     deps = import_bank()
@@ -130,12 +133,13 @@ def install_deps():
         puts(f"PKG :: {pkg}")
         os.system(f"sudo apt install {pkg} -y")
 
+
 def shell(args):
-    
+
     if len(args) < 2:
         sakuya()
         return 255
-    
+
     if args[1] == "help":
         verbose = False
         for x in args:
@@ -149,16 +153,13 @@ def shell(args):
     elif args[1] == "query":
         data = import_bank()
         query(data["general"], args)
-    else: 
+    else:
         data = import_bank()
         out = exec(query(data["general"], args))
         return out
 
     return 0
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
     shell(sys.argv)
-
-
-
-
