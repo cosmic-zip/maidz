f243b2326835985a57889545b7171bdb228e640e
Prism <36008397+prism-zip@users.noreply.github.com>
2024-06-19 22:34:24 +0000

    Develop (#1)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add book
    
    * update
    
    * update
    
    * update
    
    * add disk dump and file recover
    
    * update
    
    * update, improve output
    
    * black
    
    * update

--- maidz.py
+++ maidz.py
@@ -1,4 +1,32 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------#
+#   MaidZ Cybersecurity companion                                              #
+#   Copyright (C) 2024  Prism-zip                                              #
+#                                                                              #
+#                                                                              #
+#   This program is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU Affero General Public License as published   #
+#   by the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU Affero General Public License for more details.                        #
+#                                                                              #
+#   You should have received a copy of the GNU Affero General Public License   #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
+# -----------------------------------------------------------------------------#
+
+
 import json, os, sys, time, re
+
+
+def sakuya():
+    os.system("chafa -s 90x90 assets/sakuya_izayoi.png")
+    print(
+        "\n\033[1m\t[maidz] Cybersecurity companion\n\tUse maidz help to see all options\033[0m\n"
+    )
 
 
 def import_bank():
@@ -8,59 +36,130 @@ def import_bank():
     except json.JSONDecodeError as e:
         print(f"JSON::ERROR {e}")
 
-def puts(string: str):
-    print(f"[{string}]")
+
+def puts(string: str, color: str = ""):
+    color_emojis = {
+        "purple": "🟣",
+        "red": "🔴",
+        "green": "🟢",
+        "yellow": "🟡",
+        "blue": "🔵",
+        "orange": "🟠",
+        "white": "⚪",
+        "black": "⚫",
+    }
+    emoji = ""
+    if color != "":
+        emoji = color_emojis.get(color)
+    print(f"\033[1m{emoji} {string}\033[0m")
+
 
 def key_value(term: str, args: list):
 
-
     if len(args) < 1:
         print("args are to small")
-        
+
     for index, item in enumerate(args):
         if item == term:
             if index + 1 >= len(args):
                 puts("Incomplete key-value constrain :: return empty")
                 return ""
-            print(args[index +1])
-            return args[index +1]
+            # print(args[index +1])
+            return args[index + 1]
 
     puts(f"no value found for key: {term} :: return empty")
     return ""
-    
-    
+
+
 def query(data: dict, args: list):
-    
-    pattern = r'\s*@@(\w+)\s*'
-    
-    content = None
+
+    name = ""
+    content = ""
+    description = ""
+    pattern = r"\s*@@(\w+)\s*"
+
     for ctn in data:
         if ctn["name"] == args[1]:
             content = ctn["command"]
-    
+            name = ctn["name"]
+            description = ctn["description"]
+
     if not content:
         puts(f"bind not found for {args[1]}")
-    
-    
+        return None
+
     matches = re.findall(pattern, content)
     for match in matches:
         key = match.strip()
         value = key_value(f"--{key}", args)
-        content = content.replace(f'@@{key}', value)
-    
-    print(content)
-    return content
-    
-def exec(args: list):
-    pass
+        content = content.replace(f"@@{key}", value)
 
-def exec_batch(chunk: dict, args: list):
+    # print(content)
+    return {"name": name, "command": content, "description": description}
 
-    for q in chunk
+
+def exec(command: str) -> int:
+    puts(command["name"])
+    print(command["command"])
+    return 1
+
+
+def exec_batch(chunk: dict, args: list, delay: int = 0):
+
+    for q in chunk:
         out = query(chunk, args)
+        if delay:
+            time.sleep(delay)
         print(out)
 
 
-if __name__ == '__main__':
+def help(verbose: bool = False):
 
-    query(sys.argv)
+    data = import_bank()
+    data = data["general"]
+
+    for d in data:
+        puts(d["name"], "yellow")
+        puts(f"\n\t{d['description']}")
+        if verbose:
+            print(f"\n\t{d['command']}\n")
+        print("")
+
+
+def install_deps():
+    deps = import_bank()
+    deps = deps["deps"]
+    for pkg in deps:
+        puts(f"PKG :: {pkg}")
+        os.system(f"sudo apt install {pkg} -y")
+
+
+def shell(args):
+
+    if len(args) < 2:
+        sakuya()
+        return 255
+
+    if args[1] == "help":
+        verbose = False
+        for x in args:
+            if x == "-v":
+                verbose = True
+        help(verbose)
+    elif args[1] == "sakuya":
+        sakuya()
+    elif args[1] == "install":
+        install_deps()
+    elif args[1] == "query":
+        data = import_bank()
+        query(data["general"], args)
+    else:
+        data = import_bank()
+        out = exec(query(data["general"], args))
+        return out
+
+    return 0
+
+
+if __name__ == "__main__":
+    shell(sys.argv)
